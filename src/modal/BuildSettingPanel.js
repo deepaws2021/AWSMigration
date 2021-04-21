82a3edbb598d05a66d9fabbb312f656d3cffde66
deepaws2021 <82222019+deepaws2021@users.noreply.github.com>
2021-04-21 19:35:20 +0530

    Add files via upload

--- src/modal/BuildSettingPanel.js
+++ src/modal/BuildSettingPanel.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
 import { Modal, FormGroup, ControlLabel, FormControl, HelpBlock, Button } from 'react-bootstrap';
-import projectmapping from '../../../ProjectMapping.json';
+import projectmapping from '../ProjectMapping.json';
 
 class BuildSettingPanel extends Component{
     constructor(props){
